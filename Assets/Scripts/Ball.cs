75b987bb377b68ed2c36f6cf6ad61b17b22be155
Tamir Zehavi (Cybercortex) <57259862+TamirZehavi@users.noreply.github.com>
2020-12-26 19:33:33 +0200

    First Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //[SerializeField] private RampController ramp;
+    [SerializeField] private float randomNumber = 0f;
+    [SerializeField] private float initialSpeedX = 0f;
+    [SerializeField] private float initialSpeedY = 0f;
+
+    private Rigidbody2D rigidBody;
+
+    private bool gameStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!gameStarted)
+        {
+            gameStarted = true;
+            Physics2D.gravity = Vector2.zero;
+            rigidBody.velocity = new Vector2(initialSpeedX, initialSpeedY);
+        }
+
+        else
+        {
+            Vector2 randomVelocity = new Vector2(Random.Range(0f, randomNumber), Random.Range(0f, randomNumber));
+            rigidBody.velocity += randomVelocity;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/RampController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class RampController : MonoBehaviour
+{
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 rampPosition = new Vector2(transform.position.x, transform.position.y);
+        rampPosition.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = rampPosition;
+    }
+
+    private float GetXPos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+    }
+}
+
